c6ab76625c319c9584e41fd5bc6199877cb85db1
Rafael <rafaelsilva.fatec@gmail.com>
2018-08-31 23:32:37 -0300

    Improve video component style

--- src/app.js
+++ src/app.js
@@ -2,51 +2,51 @@
 
 import React from 'react'
 import styled, { injectGlobal } from 'styled-components'
+import VideosList from './components/videos-list'
 
 import 'normalize.css'
 import 'milligram'
-import { Div } from 'glamorous';
 
 const App = () => (
-  <Container>
-    <Header>
-      <h1>ReactFlix</h1>
-    </Header>
+	<Container>
+		<Header>
+			<h1>ReactFlix</h1>
+		</Header>
 
-    <Main>
-      Conteúdo
-    </Main>
+		<Main>
+			<VideosList />
+		</Main>
 
-    <Footer>
-      &copy;2018
+		<Footer>
+			&copy;2018
     </Footer>
-  </Container>
+	</Container>
 )
 
 injectGlobal`
    html, body, div[data-js="app"]{
-     height: 100%;
-   }
-`
+				height: 100%;
+		}
+ `
 
 const headerHeight = '60px'
 const footerHeight = '30px'
 
 const Container = styled.div`
-   height: 100%;
-`
-
+	 height: 100%;
+ `
 const Header = styled.header`
   height: ${headerHeight};
-  background: #333;
-`
+			background: #333;
+			`
+
 const Main = styled.main`
-min-height: calc(100% - ${headerHeight} - ${footerHeight});
-`
+	min-height: calc(100% - ${headerHeight} - ${footerHeight});
+		`
 
 const Footer = styled.footer`
-height: ${footerHeight};
-background: #333;
-`
+  height: ${footerHeight};
+			background: #333;
+		`
 
 export default App
