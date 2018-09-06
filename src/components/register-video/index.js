d0b4b37b26727829ba1f60e081a1786f553d26d0
Rafael <rafaelsilva.fatec@gmail.com>
2018-09-05 23:52:57 -0300

    Reset form after submit

--- src/components/register-video/index.js
+++ src/components/register-video/index.js
@@ -22,18 +22,19 @@ const Form = styled.form`
 `
 
 const mapDispatchToProps = (dispatch) => ({
-    onSubmit: (e) => {
+    onSubmit: async (e) => {
         e.preventDefault()
+        e.persist()
 
         const {
             id: { value: id },
             title: { value: title }
         } = e.target
 
-        dispatch(registerVideo({ id, title }))
+        await dispatch(registerVideo({ id, title }))
 
-        e.target.id.value = ''
-        e.target.title.value = ''
+        e.target.reset()
+        e.target[0].focus()
     }
 })
 
